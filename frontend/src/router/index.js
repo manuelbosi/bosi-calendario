484de22846d9f13c3dd0db629fbb556047c7ce4d
Manuel Bosi <manuel.bosi@webscape.it>
2021-12-10 15:08:48 +0100

    set authenticated flag false on logout

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Dashboard from "../views/Dashboard";
+import store from "../store";
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+  { path: '/dashboard', name: 'Dashboard', component: Dashboard, meta: { requireAuth: true } },
 
 ]
 
@@ -12,5 +13,29 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+function isLogged() {
+  return store.getters.getIsLogged
+}
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requireAuth)) {
+    if (!isLogged()) {
+      next({
+        path: "/",
+      });
+    } else {
+      if (!isLogged()) {
+        next({
+          path: "/",
+        });
+      } else {
+        next();
+      }
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
