26ea9864f43a9034b63fcfa14ed6ccdd9805e2f2
Manuel Bosi <manuel.bosi@webscape.it>
2021-12-10 16:34:56 +0100

    set authenticated flag false on logout

--- frontend/src/api/User.js
+++ frontend/src/api/User.js
@@ -1,13 +1,15 @@
-// import router from "../router";
-// import store from "../store";
 import Api from "./Api";
 import router from "../router";
+import store from "../store";
 
 const User = () => {
 
     const logout = () => {
         Api.post('/logout').then(res => {
-            if (res.status === 200) router.push({name: 'Home'})
+            if (res.status === 200) {
+                store.dispatch('setAuthenticated', false)
+                router.push({name: 'Home'})
+            }
         }).catch(err => {
             console.log(err)
         })
