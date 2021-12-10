96ea8af744ba84ce2e71a3160deb7b642e09374a
Manuel Bosi <manuel.bosi@webscape.it>
2021-12-10 16:36:25 +0100

    add loader on axios api call

--- frontend/src/api/Api.js
+++ frontend/src/api/Api.js
@@ -1,8 +1,21 @@
 import axios from "axios";
+import * as NProgress from "nprogress/nprogress";
 
 const Api = axios.create({
     withCredentials: true,
     baseURL: 'http://localhost:8000/api',
+})
+
+// before a request is made start the nprogress
+Api.interceptors.request.use(config => {
+    NProgress.start()
+    return config
+})
+
+// before a response is returned stop nprogress
+Api.interceptors.response.use(response => {
+    NProgress.done()
+    return response
 })
 
 export default Api
