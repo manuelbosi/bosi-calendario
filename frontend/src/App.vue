b4c27e4c5ce76dca06c3ee086ffd648f0a20cb49
Manuel Bosi <manuel.bosi@webscape.it>
2021-12-11 16:50:02 +0100

    removed unused code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,23 +3,7 @@
 </template>
 
 <script>
-// import {onBeforeMount} from "vue";
-// import CsrfToken from "./api/CsrfToken";
-// import Cookies from 'js-cookie/dist/js.cookie.min'
-
-export default {
-  // setup() {
-  //
-  //   onBeforeMount(() => {
-  //     const hasCsrfToken = Cookies.get('XSRF-TOKEN')
-  //     if (!hasCsrfToken) setupToken()
-  //   })
-  //
-  //   const setupToken = async () => {
-  //     await CsrfToken().getCookieToken()
-  //   }
-  // }
-}
+export default {}
 </script>
 
 <style>
