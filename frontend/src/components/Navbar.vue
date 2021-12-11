441aff4ed3c5198db5f2dc59839ef83ed9a3eb5a
Manuel Bosi <manuel.bosi@webscape.it>
2021-12-11 17:02:51 +0100

    navbar refactoring

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,12 +1,6 @@
 <template>
   <nav class="p-3">
     <navbar-menu></navbar-menu>
-<!--    <ul class="p-0">-->
-<!--      <li><router-link to="/dashboard" class="mb-3 p-3"><i class="fas fa-chart-line"></i><span>Dashboard</span></router-link></li>-->
-<!--      <li><router-link to="/calendar" class="mb-3 p-3"><i class="fas fa-calendar-alt"></i><span>Calendario</span></router-link></li>-->
-<!--      <li><router-link to="/customers" class="mb-3 p-3"><i class="fas fa-users"></i><span>Clienti</span></router-link></li>-->
-<!--      <li><router-link to="/notes" class="mb-3 p-3"><i class="fas fa-clipboard"></i><span>Note</span></router-link></li>-->
-<!--    </ul>-->
   </nav>
 </template>
 
--- frontend/src/components/NavbarMenu.vue
+++ frontend/src/components/NavbarMenu.vue
@@ -1,18 +1,18 @@
 <template>
   <ul class="p-0">
-    <navbar-menu-element text="Dashboard" icon="fas fa-chart-line" route="/dashboard"></navbar-menu-element>
-    <navbar-menu-element text="Calendario" icon="fas fa-calendar-alt" route="/calendar"></navbar-menu-element>
-    <navbar-menu-element text="Clienti" icon="fas fa-users" route="/customers"></navbar-menu-element>
-    <navbar-menu-element text="Note" icon="fas fa-clipboard" route="/notes"></navbar-menu-element>
+    <navbar-link text="Dashboard" icon="fas fa-chart-line" route="/dashboard"></navbar-link>
+    <navbar-link text="Calendario" icon="fas fa-calendar-alt" route="/calendar"></navbar-link>
+    <navbar-link text="Clienti" icon="fas fa-users" route="/customers"></navbar-link>
+    <navbar-link text="Note" icon="fas fa-clipboard" route="/notes"></navbar-link>
   </ul>
 </template>
 
 <script>
-import NavbarMenuElement from "./NavbarMenuElement";
+import NavbarLink from "./NavbarLink";
 
 export default {
   name: "NavbarMenu",
-  components: { NavbarMenuElement }
+  components: { NavbarLink },
 }
 </script>
 
