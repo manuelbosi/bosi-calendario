d67e1d19307acf86fa280da6af56c98cd0b77936
Manuel Bosi <manuel.bosi@webscape.it>
2021-12-12 21:57:42 +0100

    add logout button and user's name in navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,20 +1,38 @@
 <template>
-  <nav class="p-3">
-    <navbar-menu></navbar-menu>
+  <nav>
+    <navbar-menu class="pe-3 pt-3 ps-3 pt-3"></navbar-menu>
+    <div id="user" class="p-2" v-cloak>
+      <span v-if="user.name">{{ user.name }}</span>
+      <span v-else>. . .</span>
+      <i class="fa fa-power-off" @click="logout"></i>
+    </div>
   </nav>
 </template>
 
 <script>
 import NavbarMenu from "./NavbarMenu";
-import {onMounted} from "vue";
+import {computed, onBeforeMount} from "vue";
+import useUser from "../composables/useUser";
+import {useStore} from "vuex";
 
 export default {
   name: "Navbar",
   components: { NavbarMenu },
   setup() {
-    onMounted(() => {
-      console.log("navbar mounted")
+
+    const store = useStore()
+    const { getUser, logout } = useUser()
+    const user = computed(() => store.getters.getAuthenticatedUser);
+
+    onBeforeMount(() => {
+      getUser()
     })
+
+    return {
+      user,
+      logout,
+    }
+
   }
 
 }
@@ -26,6 +44,21 @@ nav {
   background: #041C32;
   height: 100%;
   width: 280px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+#user {
+  background-color: #073b5b;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+i {
+  cursor: pointer;
 }
 
 </style>
