7dc605b5940582fb925d9dd38a8f85ab80a3c36b
Manuel Bosi <manuel.bosi@webscape.it>
2021-12-13 11:20:47 +0100

    added "add event" button

--- frontend/src/views/Calendar.vue
+++ frontend/src/views/Calendar.vue
@@ -1,5 +1,6 @@
 <template>
-  <FullCalendar :options="calendarOptions"></FullCalendar>
+  <FullCalendar :options="calendarOptions" id="calendar"></FullCalendar>
+  <div id="add-event"><i class="fas fa-plus"></i></div>
 </template>
 
 <script>
@@ -34,5 +35,25 @@ export default {
 </script>
 
 <style scoped>
-
+#add-event {
+  position: fixed;
+  z-index: 9999;
+  right: 20px;
+  bottom: 20px;
+  cursor: pointer;
+  padding: 5px;
+  background: #041C32;
+  color: white;
+  border-radius: 50px;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.1s linear;
+  font-size: 20px;
+}
+#add-event:hover {
+  background-color: #073b5b;
+}
 </style>
